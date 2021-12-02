ba33ee3b86ab941975645485e798138f405f5f28
buenni86 <arslan.buenyamin@gmail.com>
2021-12-02 11:44:48 +0100

    Initial

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This is a starter kit to help you build your own map for [WorkAdventure](https:/
 
 To understand how to use this starter kit, follow the tutorial at [https://workadventu.re/map-building](https://workadventu.re/map-building).
 
-![map](./map.png)
+![map](./dbImmo.png)
+![map](./theater.png)
 
 ## Installation
 
---
+++ skript.js
@@ -0,0 +1,117 @@
+var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
+var urlMusikClassic = "https://open.spotify.com/embed/playlist/37i9dQZF1DX1LCddwjCqDK?utm_source=generator";
+var urlMusikModern = "https://open.spotify.com/embed/playlist/37i9dQZF1DX3h1vasAdBTc?utm_source=generator";
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+var zoneMusikClassic = "music_classic";
+var zoneMusikModern = "music_modern";
+var zoneDirections = "directions";
+var zoneDirections1 = "directions1";
+var zoneDirections2 = "directions2";
+
+WA.room.onEnterZone(zoneMusikClassic, () => {
+   currentPopup =  WA.ui.openPopup("popUpMusicClassic","Klassische Weihnachtslieder?!",[
+        {
+            label: "Her damit!",
+			callback: (popup => {
+                WA.nav.openCoWebSite(urlMusikClassic);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneMusikClassic, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneMusikModern, () => {
+   currentPopup =  WA.ui.openPopup("popUpMusicModern","Weihnachtslieder?!",[
+        {
+            label: "Her damit!",
+			callback: (popup => {
+                WA.nav.openCoWebSite(urlMusikClassic);
+                isCoWebSiteOpened = true;
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneMusikModern, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneDirections, () => {
+   currentPopup =  WA.ui.openPopup("popUpDirections","Wegweiser:\n\nNordosten: Kleiner Weihnachtsmarkt an der Kirche\n\nSüdosten: Der mittelalterliche Weihnachtsmarkt heißet euch Willkommen!\n\nSüdwesten: Großer Parkweihnachtsmarkt\n\nNordwesten: Die Eisbahn wartet auf dich!",[
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneDirections, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneDirections1, () => {
+   currentPopup =  WA.ui.openPopup("popUpDirections1","Wegweiser:\n\nNordosten: Kleiner Weihnachtsmarkt an der Kirche\n\nSüdosten: Der mittelalterliche Weihnachtsmarkt heißet euch Willkommen!\n\nSüdwesten: Großer Parkweihnachtsmarkt\n\nNordwesten: Die Eisbahn wartet auf dich!",[
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneDirections1, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
+WA.room.onEnterZone(zoneDirections2, () => {
+   currentPopup =  WA.ui.openPopup("popUpDirections2","Wegweiser:\n\nNordosten: Kleiner Weihnachtsmarkt an der Kirche\n\nSüdosten: Der mittelalterliche Weihnachtsmarkt heißet euch Willkommen!\n\nSüdwesten: Großer Parkweihnachtsmarkt\n\nNordwesten: Die Eisbahn wartet auf dich!",[
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]);
+})
+
+WA.room.onLeaveZone(zoneDirections2, () =>{
+    closePopUp();
+
+    if (isCoWebSiteOpened) {
+        WA.nav.closeCoWebSite();
+        isCoWebSiteOpened = false;
+    }
+})
+
